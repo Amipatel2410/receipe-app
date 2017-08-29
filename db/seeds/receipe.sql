6c4fa10aa6d70ebaaff4caa85920d24279810a16
amisha patel <ami.patel2410@gmail.com>
2017-08-29 10:26:42 -0400

    add database

---
+++ db/seeds/receipe.sql
@@ -0,0 +1,27 @@
+\c receipe-app-development;
+
+INSERT INTO receipe(title, picture, direction) VALUES
+  ('Grill Sandwitch','https://challengedairy.com/files/recipe_images/recipe_tuscan_grilled_cheese.jpg',
+    'Preheat Breakfast Sandwich Maker until green PREHEAT light comes on. Lightly butter one side of each bread circle.
+    Place one bread circle, buttered-side down in bottom ring of Breakfast Sandwich Maker. Top with cheese.
+    Lower cooking plate and top ring.Close cover.Slide out cooking plate by rotating handle clockwise.');
+
+INSERT INTO receipe(title, picture, direction) VALUES
+  ('Panipuri','https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Home_made_Indian_Panipuri.jpg/294px-Home_made_Indian_Panipuri.jpg',
+    'Take a bowl and Mix semolina,maida,baking soda and salt in it
+Add water little by littel and bind stiff dough. Cover it with wet cloth and set aside for 25 minutes.
+Remove cloth and knead again until becomes smooth.Divide dough into equal portion and roll it using rolling pin into round shaped roti.
+Cut roti in small round puris using a round shaped small lid.');
+
+INSERT INTO receipe(title, picture, direction) VALUES
+  ('Burger','https://challengedairy.com/files/recipe_images/recipe_tuscan_grilled_cheese.jpg',
+    'Preheat Breakfast Sandwich Maker until green PREHEAT light comes on. Lightly butter one side of each bread circle.
+    Place one bread circle, buttered-side down in bottom ring of Breakfast Sandwich Maker. Top with cheese.
+    Lower cooking plate and top ring.Close cover.Slide out cooking plate by rotating handle clockwise.');
+
+INSERT INTO receipe(title, picture, direction) VALUES
+  ('Panjabi','https://i2.wp.com/insighthimachal.com/wp-content/uploads/2016/06/punjabi-food.jpeg?fit=700%2C375',
+    'Preheat Breakfast Sandwich Maker until green PREHEAT light comes on. Lightly butter one side of each bread circle.
+    Place one bread circle, buttered-side down in bottom ring of Breakfast Sandwich Maker. Top with cheese.
+    Lower cooking plate and top ring.Close cover.Slide out cooking plate by rotating handle clockwise.');
+
